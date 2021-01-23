0f9dba4807054a92eaa2e6dcb13e8f084955efe3
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2021-01-22 23:18:49 -0800

    Changed player models to include overlays

--- src/stats/bw.ts
+++ src/stats/bw.ts
@@ -70,7 +70,7 @@ export default async function bw(msg: Message, args: Args): Promise<void> {
       .setDescription(
         `All of the bedwars stats for \`${name}\` are placed here.`
       )
-      .setThumbnail(`https://crafatar.com/renders/body/${uuid}`)
+      .setThumbnail(`https://crafatar.com/renders/body/${uuid}?overlay`)
       .addFields(
         {
           name: `Games:`,
